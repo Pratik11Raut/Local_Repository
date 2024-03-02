4e191e9773f1dccc1c202620f4a8c6d62ac2efc1
Pratik11Raut <pratik172013@gmail.com>
2024-03-02 08:53:50 +0530

    Print Hello Java GIT & GITHUB

---
+++ Print_Hello_Basic/src/com/pratik/git/github/Printing_Hello_Basic.java
@@ -0,0 +1,11 @@
+package com.pratik.git.github;
+
+public class Printing_Hello_Basic {
+
+	public static void main(String[] args) {
+		String print = "Hello Java, GIT & GITHUB";
+		System.out.println(print);
+
+	}
+
+}
