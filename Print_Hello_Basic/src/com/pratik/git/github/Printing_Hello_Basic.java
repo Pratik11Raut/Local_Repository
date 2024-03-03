d30189d0c9b3676ff3326a4fbed33c1cfb0af142
Pratik11Raut <pratik172013@gmail.com>
2024-03-03 15:23:18 +0530

    Changes made in project

--- Print_Hello_Basic/src/com/pratik/git/github/Printing_Hello_Basic.java
+++ Print_Hello_Basic/src/com/pratik/git/github/Printing_Hello_Basic.java
@@ -1,10 +1,21 @@
 package com.pratik.git.github;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Printing_Hello_Basic {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		String print = "Hello Java, GIT & GITHUB";
 		System.out.println(print);
+		@SuppressWarnings("rawtypes")
+		List list = new ArrayList();
+		list.add(10);
+		list.add("Pratik");
+		list.add(20);
+		list.add("Raut");
+		System.out.println(list);
 
 	}
 
