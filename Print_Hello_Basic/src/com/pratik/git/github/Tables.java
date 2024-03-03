bb37ec75d382f820acc4d70f8b907d092e0eceb4
Pratik11Raut <pratik172013@gmail.com>
2024-03-03 20:56:59 +0530

    new class created

---
+++ Print_Hello_Basic/src/com/pratik/git/github/Tables.java
@@ -0,0 +1,14 @@
+package com.pratik.git.github;
+
+public class Tables {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+			int j=1;
+			int equal = i*j;
+			System.out.println(i+" * "+j+" = "+equal);
+		}
+
+	}
+
+}
